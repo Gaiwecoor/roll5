885d2de04278414256f6dc4b9147919a2c66704b
Gaiwecoor <gaiwecoor@gmail.com>
2023-07-27 14:02:38 -0500

    `roll5` naming convention

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Klink' });
+  res.render('roll5', { title: 'Klink' });
 });
 
 module.exports = router;
